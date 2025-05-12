3c8bdc07ced7f678fbea92c4ff771f1e682d5207
haimski <haimski@users.noreply.github.com>
2025-05-12 09:01:33 +0000

    items sorting not working yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import { RouterDataProvider } from './context/RoutersDataContext';
+import { RouterFilterProvider } from './context/RoutersFilterContext';
 import RouterList from './components/RouterList'; 
 import RouterFilterControls from './components/RouterFilterControls';
+import RouterDetails from './components/RouterDetails';
 import './App.css';
 
+interface Router {
+  id: string;
+  name: string;
+  type: string;
+  updatedAt: string;
+}
+
 function App() {
+  const [open, setOpen] = React.useState(true);
+  const [selectedRouter, setSelectedRouter] = React.useState<Router | null>(null);
+
+  interface Router {
+    id: string;
+    name: string;
+    type: string;
+    updatedAt: string;
+  }
+
+  const handleModal = (router: Router): void => {
+    if (!open) {
+      setSelectedRouter(router);
+      setOpen(true);
+      return;
+    }
+    setSelectedRouter(null);
+    setOpen(false);
+  };
+
   return (
     <RouterDataProvider>
+      <RouterFilterProvider>
       <div className="App-main">
         <h2 className="placeholder-title">Sky Rocket Routers</h2>
         <RouterFilterControls />
-        <RouterList />
+        <RouterList handleModal={() => selectedRouter && handleModal(selectedRouter)} />
+        <RouterDetails open={false} handleClose={() => {selectedRouter && handleModal(selectedRouter)}} router={{ id: '', name: '', type: '', updatedAt: '' }} />
       </div>
+      </RouterFilterProvider>
     </RouterDataProvider>
   );
 }
