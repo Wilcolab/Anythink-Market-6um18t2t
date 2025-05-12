ebbd2ca54e2a70da4cd8f7c7566cd23c1c6ff95d
haimski <haimski@users.noreply.github.com>
2025-05-12 06:47:01 +0000

    moment and env vars

--- frontend/src/components/RouterCard.tsx
+++ frontend/src/components/RouterCard.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 
 interface Router {
     id: string;
@@ -15,7 +16,7 @@ const RouterCard = ({ router }: RouterCardProps) => {
         <div key={router.id} className="router-card">
                     <h3>{router.name}</h3>
                     <p>Type: {router.type}</p>
-                    <p>Updated At: {new Date(router.updatatedAt).toLocaleDateString()}</p>
+                    <p>{moment(router.updatatedAt).format('MMMM Do YYYY')}</p>
                 </div>
     );
 }
